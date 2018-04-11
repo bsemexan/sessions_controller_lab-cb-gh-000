980cade190ffee8affcd46099ec14f7f4b5491d7
Berry  Semexan <bsemexan@gmail.com>
2018-04-11 23:31:16 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     if params[:name].nil? || params[:name].empty?
-      redirect_to new_session_path
+      redirect_to login_path
     else
       session[:name] = params[:name]
       redirect_to '/'
