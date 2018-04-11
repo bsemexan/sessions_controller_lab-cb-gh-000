f50cce2d03e0b4408977ab0fb6e42bb31a1998ba
Berry  Semexan <bsemexan@gmail.com>
2018-04-11 23:29:54 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
   def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to new_session_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
   end
 
   def destroy
